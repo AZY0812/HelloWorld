8f1ef56be30ae6ce6667f4d69da9ece72cf4134b
AZY0812 <zy_thinkdifferent@icloud.com>
2024-07-04 17:05:37 +0800

    test

---
+++ TestifElse2.java
@@ -0,0 +1,10 @@
+public class TestifElse2 {
+	public static void main(String[] args) {
+		int x = 30;
+		if(x < 20) {
+			System.out.println("这是if语句");
+		} else {
+			System.out.println("这是else语句");
+		}
+	}
+}
